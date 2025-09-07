d86b191ae17b6d4cace95f0bdae7039bcc572e27
Praveen-tej <tejpraveen2020@gmail.com>
2025-09-07 19:56:46 +0530

    Updated styles

--- Server/index.js
+++ Server/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const complaintsRoute = require("./routes/complaints");
+  
 require('dotenv').config();
 const app = express();
 
