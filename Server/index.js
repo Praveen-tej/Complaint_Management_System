1d8063333e0d07f5e4ed07432c35bda281b447a6
Praveen-tej <tejpraveen2020@gmail.com>
2025-09-09 19:35:23 +0530

    Updated styles

--- Server/index.js
+++ Server/index.js
@@ -2,21 +2,16 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const complaintsRoute = require("./routes/complaints");
-
 require('dotenv').config();
 const app = express();
-
 app.use(cors({
   origin: 'http://localhost:5173'
 }));
-
 app.use(express.json());
 app.use("/api/complaints", complaintsRoute);
-
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
-  useUnifiedTopology: true
-})
+  useUnifiedTopology: true})
 .then(() => console.log('MongoDB connected'))
 .catch(err => console.log(err));
 app.get('/', (req, res) => res.send('API Running'));
